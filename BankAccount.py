8f02e65b58d7192e271b84036260b747246f6c09
Vithusha Ravirajan <vithusharavirajan@Vithushas-MacBook-Pro.local>
2022-10-30 13:09:10 -0400

    completed all methods

--- BankAccount.py
+++ BankAccount.py
@@ -1,3 +1,6 @@
+from re import L
+
+
 class BankAccount:
     def __init__ (self, full_name, account_number, routing_number, balance):
         self.full_name = full_name
@@ -15,5 +18,18 @@ class BankAccount:
         if (self.balance < 0):
             print("Insufficient funds. This transaction incurs a $10 overdraft fee.")
             self.balance -= 10
-            print(f'Current balance is ${self.balance}')
+            
+    def get_balance(self):
+        print(f'Current account balance is ${self.balance}')
+        return self.balance
+
+    def add_interest(self):
+        self.balance *= 1.00083
+
+    def print_receipt(self):
+        print(f'{self.full_name}')
+        print(f'Account No.: {self.account_number}')
+        print(f'Routing No.: {self.routing_number}')
+        print(f'Balance: ${self.balance}')
+
 
