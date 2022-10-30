badf3b1db74d0d080a6ef1c8f7ab33b6417efff0
Vithusha Ravirajan <vithusharavirajan@Vithushas-MacBook-Pro.local>
2022-10-30 12:49:04 -0400

    defined deposit and withdraw methods

--- BankAccount.py
+++ BankAccount.py
@@ -4,3 +4,16 @@ class BankAccount:
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = balance
+    
+    def deposit (self, amount):
+        self.balance += amount
+        print(f'Amount Deposited: ${amount}')
+
+    def withdraw (self, amount):
+        self.balance -= amount
+        print(f'Amount Withdrawn: ${amount}')
+        if (self.balance < 0):
+            print("Insufficient funds. This transaction incurs a $10 overdraft fee.")
+            self.balance -= 10
+            print(f'Current balance is ${self.balance}')
+
